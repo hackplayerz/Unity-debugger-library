82d3a71b97fe01f701049a9fd9b8022889eb7939
Dev.NikuRamen <dev.alter0@gmail.com>
2022-11-11 14:36:07 +0900

    Add file

---
+++ UnityCustomDebugger/UnityCustomDebugger/UnityCustomDebugger/DEBUG.cs
@@ -0,0 +1,34 @@
+﻿namespace UnityCustomDebugger
+{
+using UnityEngine;
+
+public class DEBUG : MonoBehaviour
+    {
+        public static void Log(string message, LogType logType = LogType.Log, StackTraceLogType stackTraceLogType = StackTraceLogType.ScriptOnly)
+        {
+#if UNITY_EDITOR || UNITY
+        Application.SetStackTraceLogType(logType,stackTraceLogType);
+        Debug.Log(message);
+#endif
+        }
+
+        public static void Warning(string message, LogType logType = LogType.Log, StackTraceLogType stackTraceLogType = StackTraceLogType.ScriptOnly)
+        {
+#if UNITY_EDITOR || UNITY
+        Application.SetStackTraceLogType(logType,stackTraceLogType);
+        Debug.LogWarning(message);
+#endif
+
+        }
+
+        public static void Error(string message, bool forceQuit = false, LogType logType = LogType.Log, StackTraceLogType stackTraceLogType = StackTraceLogType.ScriptOnly)
+        {
+#if UNITY_EDITOR || UNITY
+        Application.SetStackTraceLogType(logType,stackTraceLogType);
+        Debug.LogError(message);
+        if (forceQuit)
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
+    }
+}
